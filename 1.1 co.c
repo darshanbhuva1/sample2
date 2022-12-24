2a4d1a16d76207a09f8a0df0ee8a94002fccc728
Darsh bhuva <bhuvadarshan12@gmail.com>
2022-12-24 13:23:48 +0530

    this file is C programing

---
+++ 1.1 co.c
@@ -0,0 +1,13 @@
+//this program preapad by 22CE013 darshan
+#include<stdio.h>
+int main()
+{
+    printf("%c%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c",1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1);
+    printf("\n%c\"if you are resisting, you are feeding it.\t\t\t\t     %c\n",4,4);
+    printf("%c\tany energy fight, you are feeding it.\t\t\t\t     %c\n",1,1);
+    printf("%c\t\tif you are pushing something away,\t\t\t     %c\n",3,3);
+    printf("%c\t\t\tyou are inviting it to stay\"by Michael singer.       %c\n",4,4);
+    printf("%c%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c",1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1);
+    printf("\nby darshan bhuva");
+    return 0;
+}
---
+++ 1.2 co.c
@@ -0,0 +1,27 @@
+//this program preapad by 22CE013 Darshan
+#include<stdio.h>
+int main()
+{
+    printf("#====#====#====#====#====#====#====#====#====#====#====#\n");
+    printf("\t\t\tBIO-DATA\n");
+    printf("#====#====#====#====#====#====#====#====#====#====#====#\n");
+    printf("\t\t    Basic Information\n");
+    printf("\t\t    -----------------\n");
+    printf("\t     Name     : Darshan bhuva\n");
+    printf("\t     Address  : Khajuri pipaliya\n");
+    printf("\t     Mo.Number: 799*******\n");
+    printf("\t     Gender   : Male\n");
+    printf("\t     DOB      : 30/06/2005\n");
+    printf("\t\t   Education Qualification\n");
+    printf("\t\t   -----------------------\n");
+    printf("\t%c SSC -> s.g. school\t\t-> 2020  67pr",4);
+    printf("\n\t%c HSC -> creative sci. school   -> 2022  89pr",4);
+    printf("\n\t\t   Other information\n");
+    printf("\t\t   -----------------");
+    printf("\n\t    %cskills -c logical que.sloving exc.",4);
+    printf("\n#====#====#====#====#====#====#====#====#====#====#====#\n");
+    printf("\t\t\t%cTHANK YOU%c\n",3,3);
+    printf("#====#====#====#====#====#====#====#====#====#====#====#");
+    printf("\n by darshan bbuva");
+    return 0;
+}
